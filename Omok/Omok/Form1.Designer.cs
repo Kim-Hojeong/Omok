b5e0ff4ad064caff3f6c49fe712971cbaf805a6c
Kim-Hojeong <hojeong1105@gmail.com>
2021-03-13 19:35:12 +0900

    Add files via upload
    Start!

---
+++ Omok/Omok/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Omok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.txtTurn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 800);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(858, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(858, 41);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(858, 789);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 4;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // txtTurn
+            // 
+            this.txtTurn.Location = new System.Drawing.Point(858, 70);
+            this.txtTurn.Name = "txtTurn";
+            this.txtTurn.ReadOnly = true;
+            this.txtTurn.Size = new System.Drawing.Size(75, 21);
+            this.txtTurn.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(940, 820);
+            this.Controls.Add(this.txtTurn);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.TextBox txtTurn;
+    }
+}
+
---
+++ Omok/Omok/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Omok
+{
+    public partial class Form1 : Form
+    {
+        bool IsStart;
+        int Player;
+        public Form1()
+        {
+            InitializeComponent();
+
+            IsStart = false;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+
+            for (int y = 0; y < 19; y++)
+            {
+                for (int x = 0; x < 19; x++)
+                {
+                    Point st, ed;
+
+                    st = new Point(x * 40 + 40, y * 40 + 40);
+                    ed = new Point(x * 40 + 40, 19 * 40);
+
+                    g.DrawLine(Pens.Black, st, ed);
+
+                    st = new Point(x * 40 + 40, y * 40 + 40);
+                    ed = new Point(19 * 40, y * 40 + 40);
+
+                    g.DrawLine(Pens.Black, st, ed);
+                }
+            }
+
+            g.Dispose();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (IsStart == false)
+                return;
+
+            Graphics g = panel1.CreateGraphics();
+
+            Point pt = new Point(((e.X + 20) / 40) * 40, ((e.Y + 20) / 40) * 40);
+
+            Rectangle rect = new Rectangle(pt.X - 15, pt.Y - 15, 30, 30);
+
+            if (Player == 0)
+                g.FillEllipse(Brushes.Black, rect);
+            else
+                g.FillEllipse(Brushes.White, rect);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            IsStart = true;
+
+            Player = 0;
+
+            MessageBox.Show("시작합니다.");
+
+            txtTurn.Text = string.Format("Player{0}", Player + 1);
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            if (IsStart == false)
+                return;
+
+            Player++;
+
+            Player %= 2;
+
+            txtTurn.Text = string.Format("Player{0}", Player + 1);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("종료합니까?", "", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            this.Close();
+        }
+    }
+}
